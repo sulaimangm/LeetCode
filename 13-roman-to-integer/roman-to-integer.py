83aa271b77383f7e5135761898a2ed8d736c939a
sulaimangm <42448524+sulaimangm@users.noreply.github.com>
2023-08-30 00:53:56 -0400

    Time: 42 ms (96.43%) | Memory: 16.4 MB (10.23%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        end = len(s)
+        counter = 0
+        output = 0
+        roman = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        for index, char in enumerate(s):
+            if index == len(s) - 1:
+                output += roman[char]
+            else:
+                if roman[s[index]] < roman[s[index+1]]:
+                    output -= roman[char]
+                else:        
+                    output += roman[char]
+        return output
+
+            
