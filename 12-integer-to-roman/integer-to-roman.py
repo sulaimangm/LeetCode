25a90916bb8b4fb4457b1a7a1fb3793fe396daff
sulaimangm <42448524+sulaimangm@users.noreply.github.com>
2023-08-30 03:30:52 -0400

    Time: 42 ms (96.40%) | Memory: 16.2 MB (98.75%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        string = ""
+        roman = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D",  900: "CM", 1000: "M"}
+        for i in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            while num >= i:
+                num -= i
+                string += roman[i]
+        return string
+        
