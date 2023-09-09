df658c191854a8ee9e0f6dc083a869cd3067d126
sulaimangm <42448524+sulaimangm@users.noreply.github.com>
2023-09-09 15:58:45 +0530

    Time: 49 ms (85.32%) | Memory: 16.9 MB (60.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = sorted(s)
-        t = sorted(t);
+        # s = sorted(s)
+        # t = sorted(t);
+        # if len(s) != len(t):
+        #     return False;
+        # for i in range(len(s)):
+        #     if (s[i] != t[i]):
+        #         return False;
+        # return True;
+        # Doing character counter is auguably better
         if len(s) != len(t):
-            return False;
-        for i in range(len(s)):
-            if (s[i] != t[i]):
-                return False;
-        return True;
+            return False
+        s_counts = {}
+        t_counts = {};
+        for i in s:
+            s_counts[i] = s_counts.get(i, 0) + 1
+        for i in t:
+            t_counts[i] = t_counts.get(i, 0) + 1
+
+        if s_counts == t_counts:
+            return True
+        return False
