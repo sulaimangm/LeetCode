60a8fab4779e92ee2de08813d8de03e175aa77eb
sulaimangm <42448524+sulaimangm@users.noreply.github.com>
2023-09-17 13:19:02 +0300

    Time: 92 ms (30.63%), Space: 17.1 MB (44.8%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.py
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -2,7 +2,6 @@ class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         rLen = len(mat)
         cLen = len(mat[0])
-        counter = 0
         temp = []
         newmat = []
         if rLen*cLen != r*c:
@@ -10,9 +9,7 @@ class Solution:
         for i in range(rLen):
             for j in range(cLen):
                 temp.append(mat[i][j])
-                counter += 1
-                if counter == c:
+                if len(temp) == c:
                     newmat.append(temp)
                     temp = []
-                    counter = 0
         return newmat
