684ca6e1fbd3df6c2e7ff5c7e7d6e2027438293e
sulaimangm <42448524+sulaimangm@users.noreply.github.com>
2023-09-24 19:56:00 +0300

    Time: 29 ms (95.22%) | Memory: 16.3 MB (32.28%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        matches = {}
+        s = s.split()
+        words = []
+        
+        if len(pattern) != len(s):
+            return False
+        
+        for i in range(len(pattern)):
+            if pattern[i] in matches:
+                if matches[pattern[i]] != s[i]:
+                    return False
+            else:
+                if s[i] in words:
+                    return False
+                matches[pattern[i]] = s[i]
+                words.append(s[i])
+        return True   
