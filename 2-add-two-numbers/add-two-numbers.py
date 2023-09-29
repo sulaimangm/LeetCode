cb785843d1e39f92bb736d914f8d9814076e7952
sulaimangm <42448524+sulaimangm@users.noreply.github.com>
2023-09-29 17:33:53 +0300

    Time: 63 ms (67.75%) | Memory: 16.2 MB (97.83%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,39 +5,18 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        sums = 0
+        carry = 0
         output = ListNode()
         temp = output
-        while l1 and l2:
-            sums += l1.val + l2.val
-            temp.val = sums % 10
-            sums = sums // 10
-            l1 = l1.next
-            l2 = l2.next
-            if l1 or l2:
-                temp.next = ListNode()
-                temp = temp.next
-            
-        while l1:
-            sums += l1.val
-            temp.val = sums % 10
-            sums = sums // 10
-            l1 = l1.next
-            if l1:
-                temp.next = ListNode()
-                temp = temp.next
-        while l2:
-            sums += l2.val
-            temp.val = sums % 10
-            sums = sums // 10
-            l2 = l2.next
-            if l2:
-                temp.next = ListNode()
-                temp = temp.next
-        print(sums)
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y  = l2.val if l2 else 0
 
-        if sums > 0:
-            temp.next = ListNode()
+            sums = carry + x + y
+            carry = sums//10
+            temp.next = ListNode(sums % 10)
+
             temp = temp.next
-            temp.val = 1
-        return output
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return output.next
